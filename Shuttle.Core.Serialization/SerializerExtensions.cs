6cdd4801b21d18c430cbe74ddf5ed22621869b8c
Eben <me@ebenroux.co.za>
2023-09-16 08:44:42 +0200

    - added extensions for deserializing to a type

---
+++ Shuttle.Core.Serialization/SerializerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Shuttle.Core.Contract;
+
+namespace Shuttle.Core.Serialization
+{
+    public static class SerializerExtensions
+    {
+        public static T Deserialize<T>(this ISerializer serializer, Stream stream)
+        {
+            return (T)Guard.AgainstNull(serializer, nameof(serializer)).Deserialize(typeof(T), Guard.AgainstNull(stream, nameof(stream)));
+        }
+
+        public static async Task<T> DeserializeAsync<T>(this ISerializer serializer, Stream stream)
+        {
+            return (T)(await Guard.AgainstNull(serializer, nameof(serializer)).DeserializeAsync(typeof(T), Guard.AgainstNull(stream, nameof(stream))));
+        }
+    }
+}
