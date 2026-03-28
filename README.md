dfa7922a62c0f0b3d5e2dfe135306e1ea2a8dfd0
Eben <me@ebenroux.co.za>
2026-03-28 11:40:51 +0200

    21.0.1-rc3 / Documentation.

--- README.md
+++ README.md
@@ -1,48 +1,95 @@
 # Shuttle.Core.Serialization
 
+The `Shuttle.Core.Serialization` package provides a consistent interface for serializing and deserializing objects.
+
 ## Installation
 
 ```bash
 dotnet add package Shuttle.Core.Serialization
 ```
 
-The following implementation of the `ISerializer` interface is used to serialize objects into a `Stream`:
+## Interface
 
-- `JsonSerializer` makes use of the `System.Text.Json` serialization functionality.
+The core of the library is the `ISerializer` interface:
+
+```csharp
+public interface ISerializer
+{
+    string Name { get; }
+    Task<Stream> SerializeAsync(object instance, CancellationToken cancellationToken = default);
+    Task<object> DeserializeAsync(Type type, Stream stream, CancellationToken cancellationToken = default);
+}
+```
+
+The following implementation is provided:
+
+- `JsonSerializer`: uses `System.Text.Json` for serialization.
 
 ## Usage
 
-### JsonSerializer
+### AddJsonSerializer
 
-``` c#
-services.AddJsonSerializer(builder => {
-	builder.Options = new JsonSerializerOptions 
-	{
-	};
+To register the `JsonSerializer`, use the `AddJsonSerializer` extension method on `IServiceCollection`:
 
-	// or
-
-	builder.Options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+```csharp
+services.AddJsonSerializer(builder => 
+{
+    builder.Options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 });
 ```
 
 The `builder.Options` is of type [JsonSerializerOptions](https://docs.microsoft.com/en-us/dotnet/api/system.text.json.jsonserializeroptions?view=net-6.0).
 
+### ISerializerService
+
+The `ISerializerService` can be used to manage multiple serializers:
+
+```csharp
+public interface ISerializerService
+{
+    IEnumerable<ISerializer> Serializers { get; }
+    ISerializerService Add(ISerializer serializer);
+    bool Contains(string name);
+    ISerializer Get(string name);
+}
+```
+
+You can use the `SerializerService` implementation to store and retrieve serializers by their `Name`:
+
+```csharp
+var serializerService = new SerializerService();
+
+serializerService.Add(new JsonSerializer(Options.Create(new JsonSerializerOptions())));
+
+if (serializerService.Contains("Json"))
+{
+    var serializer = serializerService.Get("Json");
+}
+```
+
 ## Methods
 
-### Serialize
+### SerializeAsync
 
-``` c#
+```csharp
 Task<Stream> SerializeAsync(object instance, CancellationToken cancellationToken = default);
 ```
 
 Returns the `object` as a `Stream`.
 
-### Deserialize
+### DeserializeAsync
 
-``` c#
+```csharp
 Task<object> DeserializeAsync(Type type, Stream stream, CancellationToken cancellationToken = default);
 ```
 
-Deserializes the `Stream` into an `object` of the given type.
+Deserializes the `Stream` into an `object` of the given `Type`.
+
+### DeserializeAsync&lt;T&gt; (Extension method)
+
+```csharp
+Task<T> DeserializeAsync<T>(Stream stream, CancellationToken cancellationToken = default);
+```
+
+Deserializes the `Stream` into an `object` of the given type `T`.
 
--- Shuttle.Core.Serialization/ServiceCollectionExtensions.cs
+++ Shuttle.Core.Serialization/ServiceCollectionExtensions.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Shuttle.Core.Contract;
 
 namespace Shuttle.Core.Serialization;
@@ -18,7 +18,41 @@ public static class ServiceCollectionExtensions
 
             services.AddSingleton<ISerializer, JsonSerializer>();
 
-            services.AddSingleton(Options.Create(jsonSerializerBuilder.Options));
+            services.Configure<JsonSerializerOptions>(options =>
+            {
+                options.AllowDuplicateProperties = jsonSerializerBuilder.Options.AllowDuplicateProperties;
+                options.AllowOutOfOrderMetadataProperties = jsonSerializerBuilder.Options.AllowOutOfOrderMetadataProperties;
+                options.AllowTrailingCommas = jsonSerializerBuilder.Options.AllowTrailingCommas;
+                options.PropertyNamingPolicy = jsonSerializerBuilder.Options.PropertyNamingPolicy;
+                options.PropertyNameCaseInsensitive = jsonSerializerBuilder.Options.PropertyNameCaseInsensitive;
+                options.DictionaryKeyPolicy = jsonSerializerBuilder.Options.DictionaryKeyPolicy;
+                options.DefaultIgnoreCondition = jsonSerializerBuilder.Options.DefaultIgnoreCondition;
+                options.IgnoreReadOnlyProperties = jsonSerializerBuilder.Options.IgnoreReadOnlyProperties;
+                options.IgnoreReadOnlyFields = jsonSerializerBuilder.Options.IgnoreReadOnlyFields;
+                options.IncludeFields = jsonSerializerBuilder.Options.IncludeFields;
+                options.NumberHandling = jsonSerializerBuilder.Options.NumberHandling;
+                options.UnknownTypeHandling = jsonSerializerBuilder.Options.UnknownTypeHandling;
+                options.UnmappedMemberHandling = jsonSerializerBuilder.Options.UnmappedMemberHandling;
+                options.PreferredObjectCreationHandling = jsonSerializerBuilder.Options.PreferredObjectCreationHandling;
+                options.RespectNullableAnnotations = jsonSerializerBuilder.Options.RespectNullableAnnotations;
+                options.RespectRequiredConstructorParameters = jsonSerializerBuilder.Options.RespectRequiredConstructorParameters;
+                options.ReadCommentHandling = jsonSerializerBuilder.Options.ReadCommentHandling;
+                options.MaxDepth = jsonSerializerBuilder.Options.MaxDepth;
+                options.WriteIndented = jsonSerializerBuilder.Options.WriteIndented;
+                options.IndentCharacter = jsonSerializerBuilder.Options.IndentCharacter;
+                options.IndentSize = jsonSerializerBuilder.Options.IndentSize;
+                options.NewLine = jsonSerializerBuilder.Options.NewLine;
+                options.Encoder = jsonSerializerBuilder.Options.Encoder;
+                options.DefaultBufferSize = jsonSerializerBuilder.Options.DefaultBufferSize;
+                options.ReferenceHandler = jsonSerializerBuilder.Options.ReferenceHandler;
+
+                foreach (var converter in jsonSerializerBuilder.Options.Converters)
+                {
+                    options.Converters.Add(converter);
+                }
+
+                options.TypeInfoResolver = jsonSerializerBuilder.Options.TypeInfoResolver;
+            });
 
             return services;
         }
